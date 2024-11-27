659531d8389911fef3bc28afe87584e6d281a00c
felicedev <email@felicedev.com>
2024-11-27 18:35:32 +0100

    git ignore

---
+++ encryptedconfig/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="encryptedconfig",
+    version="1.0.0",
+    description="Encrypted configuration manager supporting multiple formats.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="FeliceDev",
+    author_email="email@felicedev.com",
+    url="https://github.com/felicedev/encryptedconfig",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography",
+        "PyYAML",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
